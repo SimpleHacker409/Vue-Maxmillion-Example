960eb6a3488fee3daecf2cb55319d0c36c3e7800
SimpleHacker409 <manjunaths2705@gmail.com>
2021-02-23 03:06:33 +0100

    Initial commit

---
+++ csv2json/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <button v-on:click="csv2json">conver csv to json</button>
+<p v-for:="cat in output">{{cat.name}}</p>
+<p>{{ output }}</p>
+
+
+<select name="cats" id="cats">
+  <option v-for:="cat in output" value="c">{{ cat.name }}</option>
+</select>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+ let csvToJson = require ('convert-csv-to-json');
+
+
+// import csvtojson from 'csvtojson'
+// import fs from 'fs'
+// const csvfilepath = 'careplans.csv'
+
+
+export default {
+
+  name: 'App',
+  components: {
+    // HelloWorld
+    // csvToJson,
+ 
+  },
+  data(){
+    
+    return{
+message: 'Hi',
+fileInputName : 'careplans.csv',
+output:[
+{"name": "cat1", "age":"22"},
+{"name": "cat2", "age":"22"},
+{"name": "cat3", "age":"22"}
+],
+
+};
+  },
+  methods:{
+  csv2json(){
+     
+  this.output = csvToJson.getJsonFromScv(this.fileInputName);
+  for (let i = 0; i < this.output.length; i++) {
+    this.output.push(this.output[i])
+    
+  }
+
+   
+  }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ logo/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" v-bind:width="size" v-bind:height="size" >
+  <button v-on:click="logosmall"> small </button>
+  <button v-on:click="logomedium"> Medium </button>
+  <button v-on:click="logolarge"> Large </button>
+  <input type="text" v-model="size"/>
+  
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+    },
+    data(){
+      return{
+        width:"300",
+        height:"300",
+        size:"300"
+
+      };
+    },
+    methods:{
+      logosmall(){
+        this.width = "100"
+        this.height="100"
+
+      },
+      logomedium(){
+        this.width="200"
+        this.height="200"
+      },
+      logolarge(){
+        this.width="400"
+        this.height="400"
+      }
+          }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
